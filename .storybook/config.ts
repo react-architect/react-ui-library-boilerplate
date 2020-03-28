41cfe90eb63f002956662900a76e9640ab052b38
Frank Zickert <webmaster@clued-up.de>
2020-03-28 14:38:01 +0100

    Knobs-Button works

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -1,15 +1,20 @@
-import * as storybook from '@storybook/react';
 //import 'storybook-addon-specifications/register';
 //import * as enzyme from 'enzyme';
 //import * as Adapter from 'enzyme-adapter-react-16';
 
+import { configure, addDecorator } from '@storybook/react';
+import { withKnobs } from '@storybook/addon-knobs';
+
+
 // automatically import all files ending in *.stories.tsx in src/components
 const req = require.context('../src', true, /\.stories\.tsx$/);
+
+addDecorator(withKnobs);
 
 function loadStories() {
     req.keys().forEach(req);
 }
 
-storybook.configure(loadStories, module);
+configure(loadStories, module);
 
 //enzyme.configure({ adapter: new Adapter() });
--- src/Button.stories.tsx
+++ src/Button.stories.tsx
@@ -2,7 +2,19 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { Button } from './Button';
 
+
+import { button } from '@storybook/addon-knobs';
+
+const label = 'Do Something';
+const handler = () => console.log('foobar');
+const groupId = 'GROUP-ID1';
+
+
+
 storiesOf('Button', module)
     .add('simple button', () => (
-        <Button text="Click me twice" />
+        <div>
+            <Button text="Click me twice" />
+            {button(label, handler, groupId)}
+        </div>
     ));
